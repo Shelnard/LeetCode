76c7fbfc2a7e9b1aadb719aaac0fc41aedb87125
Shelnard <562838473@qq.com>
2019-09-08 22:35:10 +0800

    Two_Sum

--- Integer_Reverse.py
+++ Integer_Reverse.py
@@ -1,22 +1,13 @@
 #给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转
-def integer_reverse(num):
-    num_bits = []
+def integer_reverse(self, num):
     sign = 1
-
     if num < 0:
         sign = -1
-
     num = abs(num)
-
-    while num != 0:
-        num_bits.append(num / 10)
-        num = num % 10
-
-    num_reverse_bits = num_bits.reverse()
     result = 0
-
-    for i in num_reverse_bits:
-        result = result * 10 + i
+    while num:
+        result = result*10 + num%10
+        num = int(num/10)
 
     result = result * sign
 
